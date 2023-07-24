6f847a2939fd6ff6307aaa3943bc4f7d77b89e21
laurhimms <lauriehimmel1@gmail.com>
2023-07-24 12:44:23 -0700

    Player guess button + color update

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 <!-- main header for entire game -->
 # MasterMind 
-A codebreaking game for two! Will you find satisfaction in beating the bot? Or will your codebreaking attempts be thwarted?
+A codebreaking game for two! As a frequently played game in my childhood, Mastermind was the obvious choice for me to build with my newly developed and continutally evolving JS skills.  
+  
+Invented in the 70s by Mordecai Meirowitz, Mastermind was initially based on a paper-based game called Bulls and Cows. Immensely popular in its first decade, the game would sell over 30 million copies before eventually fading away to its lesser known status.
 
-<!-- ☐ Screenshot(s): Images of your actual game. -->
+Though Mastermind is typically a two-player game, I will be making a one-player, one-computer version, where the player must guess the computer's randomly generated code. Will you find satisfaction in beating the bot? Or will your codebreaking attempts be thwarted?
+
 ## Gameplay - Screenshots
 ![Wireframe screenshot](/wireframe3.png)
 
--- js/scratch.js
+++ js/scratch.js
@@ -19,22 +19,66 @@ console.log('howdy!');
 
 /*----- cached elements  -----*/
 let colorBtnEls = document.querySelectorAll('.color-buttons > button');
-let orange = colorBtnEls[1].classList[0]; 
 let guess1 = document.getElementById('guess1');
-console.log(guess1);
-
+let guessEls = document.querySelectorAll('#player-guess > section');
+console.log('guesses', guessEls)
+// let guesses = document.getElementById
 /*----- event listeners -----*/
 colorBtnEls.forEach(btn=>btn.addEventListener('click', handleBtnClick));
 
+
 /*----- functions -----*/
-function handleBtnClick(e) { 
-  let newClass = e.target.getAttribute('id')
-  guess1.setAttribute('class', newClass)
-  console.log(guess1)
-};
 
 
-handleBtnClick();
+  function handleBtnClick(e) { 
+      // let frontEl = document.querySelector('.color-buttons > button > span');
+      // console.log('frontEl', frontEl);
+      // let frontClass = frontEl.getAttribute('class');
+      // console.log('frontClass', frontClass);
+      // let colorClass = e.target.getAttribute('id');
+      // console.log('colorClass', colorClass);
+      // let guessChild = document.querySelector('.color-buttons > button').children;
+      // console.log('guessChild', guessChild)
+      // let child = ;
+      // console.log(child);
+      // guess1.appendChild(child);
+      // guess1.className = colorClass;
+      // console.log('guess1', guess1);
+      // console.log('guess1', guess1);
+      // let classEl = document.querySelector('.color-buttons > button');
+      // console.log('classEl', classEl)
+      // let correctClass = e.target.getAttribute('id');
+      // console.log('correctClass', correctClass);  
+      // console.log('guess1', guess1);
+      // let child = document.querySelector('.color-buttons > button > span');
+      // let childClass = child.getAttribute('class');
+      // console.log('childClass', childClass);
+      // console.log('guess1', guess1);
+      // guess1.className = correctClass;
+      // console.log('guess1', guess1);
+      // let node = document.createElement('span');
+      // document.getElementById('guess1').appendChild(node);
+      // node.className = childClass;
+      // console.log(guess1);
+      // console.log(e.target.style.background);
+      // guess1.style.background = e.target.background
+      // console.log(guess1)
+    let newClass = e.target.getAttribute('class');
+    console.log(newClass);
+    // return(newClass);
+    // guess1.className = newClass;
+    // console.log(guess1);
+    guessEls.forEach(section=>section.addEventListener('click', updateGuess))
+    function updateGuess(gNum) {
+      gNum.target.className = newClass;
+      console.log(gNum.target);
+   }
+  };
 
 
+// function updateGuess(gNum) {
 
+// }
+
+// updateGuess();
+// handleBtnClick()
