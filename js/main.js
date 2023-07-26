e28fadd5f5f356f138dda8f94013bc4b73a1f29a
laurhimms <lauriehimmel1@gmail.com>
2023-07-25 23:31:42 -0700

    pushing big update, mastermind attempt 2 with focus on MVP

--- js/main.js
+++ js/main.js
@@ -1,25 +1,129 @@
 console.log('howdy!');
-let colorBtnEls = document.querySelectorAll('.color-buttons > button');
-let guessEls = document.querySelectorAll('#player-guess > section');
-let submitBtn = document.querySelector('.submit')
 
 
-let guess1 = document.getElementById('guess1')
-let guess2 = document.getElementById('guess2')
-let guess3 = document.getElementById('guess3')
-let guess4 = document.getElementById('guess4')
+/*----- constants -----*/
+let colorArray = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+let cGuess1;
+let cGuess2;
+let cGuess3;
+let cGuess4;
 
-colorBtnEls.forEach(btn=>btn.addEventListener('click', handleBtnClick));
+function cArray() {let cArray =  [(colorArray[Math.floor(Math.random() * 5)+1]), (colorArray[Math.floor(Math.random() * 5)+1]), (colorArray[Math.floor(Math.random() * 5)+1]), (colorArray[Math.floor(Math.random() * 5)+1]), ];
+console.log(cArray);
+cGuess1 = document.querySelector('.cpuCode-box').children[0];
+cGuess2 = document.querySelector('.cpuCode-box').children[1];
+cGuess3 = document.querySelector('.cpuCode-box').children[2];
+cGuess4 = document.querySelector('.cpuCode-box').children[3];
 
-submitBtn.disabled = true;
+cGuess1.className = `fill-${cArray[0]}-circle`;
+cGuess2.className = `fill-${cArray[1]}-circle`;
+cGuess3.className = `fill-${cArray[2]}-circle`;
+cGuess4.className = `fill-${cArray[3]}-circle`;}
 
-function handleBtnClick(e) { 
-    let newClass = e.target.getAttribute('class');
-    guessEls.forEach(section=>section.addEventListener('click', updateGuess))
-    function updateGuess(guessChoice) {
-        guessChoice.target.className = newClass;
-    if ((guess1.getAttribute('class').includes('fill')) && ((guess2.getAttribute('class').includes('fill'))) &&((guess3.getAttribute('class').includes('fill'))) &&((guess4.getAttribute('class').includes('fill')))) {
-        submitBtn.disabled = false;
-      } else {};
+cArray();
+
+
+
+
+// ******************
+// TO DO!!!!!! when you click the player guess circles before choosing a color, they disappear!!!
+// reset submit click count when player wins
+// reset guess row classes when player wins
+// ******************
+
+/*----- state variables -----*/
+let targetClass;
+let target;
+let newBackground;
+let newClassGuess1;
+let newClassGuess2;
+let newClassGuess3;
+let newClassGuess4;
+let count = 0;
+let getRow;
+let circle1;
+let circle2;
+let circle3;
+let circle4;
+let newCrackedCount;
+
+
+/*----- cached elements  -----*/
+let turnsCount = document.getElementById('turnsCount');
+turnsCount.innerText = 0;
+
+let crackedCount = document.getElementById('codesCracked');
+crackedCount.innerText = 0;
+
+
+let thwartedCount = document.getElementById('thwarted');
+thwartedCount.innerText = 0;
+
+let colorButtonEls = document.querySelectorAll('.color-buttons button')
+
+let circleEls = document.querySelectorAll('.player-guesses .guess')
+
+let submitBtnEl = document.querySelector('.submit')
+
+let guess1El = (document.getElementById('guess1'));
+let guess2El = (document.getElementById('guess2')); 
+let guess3El = (document.getElementById('guess3'));
+let guess4El = (document.getElementById('guess4'));
+
+/*----- event listeners -----*/
+colorButtonEls.forEach((btn) => btn.addEventListener("click", handleBtnClick));
+
+circleEls.forEach((circ) => circ.addEventListener('click', setColor))
+
+submitBtnEl.addEventListener('click', submitGuess);
+
+/*----- functions -----*/
+disableSubmit()
+function handleBtnClick(e) {
+    targetClass = e.target.getAttribute('class')
+}
+
+function setColor(circ) {
+    circ.target.className = `${targetClass}-circle`;
+    // console.log(document.getElementById('guess1'))
+    freeSubmit()
+}
+
+function disableSubmit() {
+    submitBtnEl.disabled = true;
+}
+
+function toggleSubmit() {
+    if (submitBtnEl.disabled = true){submitBtnEl.id = 'disabled-button'} else {submitBtnEl.id = 'notdisabled'}
+}
+
+function freeSubmit() {
+    if (guess1El.className.includes('fill') && guess2El.className.includes('fill') && guess3El.className.includes('fill') && guess4El.className.includes('fill')) {submitBtnEl.disabled = false}
+}
+
+function submitGuess() {
+    count++;
+    console.log(count);
+    turnsCount.innerText = count;
+    getRow = document.getElementById(`row${count}`);
+    circle1 = getRow.children[0];
+    circle2 = getRow.children[1];
+    circle3 = getRow.children[2];
+    circle4 = getRow.children[3];
+    circle1.className = guess1El.className;
+    circle2.className = guess2El.className;
+    circle3.className = guess3El.className;
+    circle4.className = guess4El.className;
+    calculateWinner()
+}
+
+function calculateWinner(){
+    submitBtnEl.disabled = true;
+    guess1El.className = 'guess';
+    guess2El.className = 'guess';
+    guess3El.className = 'guess';
+    guess4El.className = 'guess';
+    if (circle1.className === cGuess1.className && circle2.className=== cGuess2.className && circle3.className === cGuess3.className && circle4.className === cGuess4.className) {crackedCount.innerText++; alert('yay!')}
+    cArray();
 };
-};
+
--- js/scratch.js
+++ js/scratch.js
@@ -2,105 +2,128 @@ console.log('howdy!');
 
 
 /*----- constants -----*/
+let colorArray = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+let cGuess1;
+let cGuess2;
+let cGuess3;
+let cGuess4;
+
+function cArray() {let cArray =  [(colorArray[Math.floor(Math.random() * 5)+1]), (colorArray[Math.floor(Math.random() * 5)+1]), (colorArray[Math.floor(Math.random() * 5)+1]), (colorArray[Math.floor(Math.random() * 5)+1]), ];
+console.log(cArray);
+cGuess1 = document.querySelector('.cpuCode-box').children[0];
+cGuess2 = document.querySelector('.cpuCode-box').children[1];
+cGuess3 = document.querySelector('.cpuCode-box').children[2];
+cGuess4 = document.querySelector('.cpuCode-box').children[3];
+
+cGuess1.className = `fill-${cArray[0]}-circle`;
+cGuess2.className = `fill-${cArray[1]}-circle`;
+cGuess3.className = `fill-${cArray[2]}-circle`;
+cGuess4.className = `fill-${cArray[3]}-circle`;}
+
+cArray();
 
 
+
+
+// ******************
+// TO DO!!!!!! when you click the player guess circles before choosing a color, they disappear!!!
+// reset submit click count when player wins
+// reset guess row classes when player wins
+// ******************
 
 /*----- state variables -----*/
-// let pScore;
-// let cScore;
+let targetClass;
+let target;
+let newBackground;
+let newClassGuess1;
+let newClassGuess2;
+let newClassGuess3;
+let newClassGuess4;
+let count = 0;
+let getRow;
+let circle1;
+let circle2;
+let circle3;
+let circle4;
+let newCrackedCount;
 
-// let pGuess;
-// formula for computer to generate their secret code
-// const INIT_STATE = {
-//   cCode = 
-let cArray = [];
-let cNums = [1, 2, 3, 4, 5, 6]
-let colorBtnEls = document.querySelectorAll('.color-buttons > button');
-colorBtnEls.forEach((el)=>{cArray.push(el.getAttribute('class'))})
-let cCode = [];
-function cpuCode () {
-  cCode.push(cArray[Math.floor(Math.random() * 5)+1], cArray[Math.floor(Math.random() * 5)+1], cArray[Math.floor(Math.random() * 5)+1], cArray[Math.floor(Math.random() * 5)+1])
-}
-cpuCode()
-console.log(cCode)
+
 /*----- cached elements  -----*/
+let turnsCount = document.getElementById('turnsCount');
+turnsCount.innerText = 0;
 
-let guessEls = document.querySelectorAll('#player-guess > section');
-let submitBtn = document.querySelector('.submit')
-
-
-
-let guess1 = document.getElementById('guess1')
-let guess2 = document.getElementById('guess2')
-let guess3 = document.getElementById('guess3')
-let guess4 = document.getElementById('guess4')
-
-let row1circle1 = document.querySelector('.row1 :nth-child(1)')
-let row1circle2 = document.querySelector('.row1 :nth-child(2)')
-let row1circle3 = document.querySelector('.row1 :nth-child(3)')
-let row1circle4 = document.querySelector('.row1 :nth-child(4)')
+let crackedCount = document.getElementById('codesCracked');
+crackedCount.innerText = 0;
 
 
-let guessArray =[];
+let thwartedCount = document.getElementById('thwarted');
+thwartedCount.innerText = 0;
+
+let colorButtonEls = document.querySelectorAll('.color-buttons button')
+
+let circleEls = document.querySelectorAll('.player-guesses .guess')
+
+let submitBtnEl = document.querySelector('.submit')
+
+let guess1El = (document.getElementById('guess1'));
+let guess2El = (document.getElementById('guess2')); 
+let guess3El = (document.getElementById('guess3'));
+let guess4El = (document.getElementById('guess4'));
 
 /*----- event listeners -----*/
-colorBtnEls.forEach(btn=>btn.addEventListener('click', handleBtnClick));
-// colorBtnEls.forEach(btn=>btn.addEventListener('click', handleBtnClick2));
+colorButtonEls.forEach((btn) => btn.addEventListener("click", handleBtnClick));
+
+circleEls.forEach((circ) => circ.addEventListener('click', setColor))
+
+submitBtnEl.addEventListener('click', submitGuess);
+
 /*----- functions -----*/
-submitBtn.disabled = true;
+disableSubmit()
+function handleBtnClick(e) {
+    targetClass = e.target.getAttribute('class')
+}
 
-function handleBtnClick(e) { 
+function setColor(circ) {
+    circ.target.className = `${targetClass}-circle`;
+    // console.log(document.getElementById('guess1'))
+    freeSubmit()
+}
 
-  let newClass = e.target.getAttribute('class');;
-  guessEls.forEach(section=>section.addEventListener('click', updateGuess))
-  
-  function updateGuess(guessChoice) {
-  guessChoice.target.className = newClass;
+function disableSubmit() {
+    submitBtnEl.disabled = true;
+}
 
-  if ((guess1.getAttribute('class').includes('fill')) && ((guess2.getAttribute('class').includes('fill'))) &&((guess3.getAttribute('class').includes('fill'))) &&((guess4.getAttribute('class').includes('fill'))))
-  {submitBtn.disabled = false;}
+function toggleSubmit() {
+    if (submitBtnEl.disabled = true){submitBtnEl.id = 'disabled-button'} else {submitBtnEl.id = 'notdisabled'}
+}
 
-  submitBtn.addEventListener('click', submitAnswer);
-  console.log('test')
-  function submitAnswer() {
-    let guessArray = [guess1.getAttribute('class'), guess2.getAttribute('class'), guess3.getAttribute('class'), guess4.getAttribute('class')];
-    row1circle1.className = guessArray[0];
-    row1circle2.className = guessArray[1];
-    row1circle3.className = guessArray[2];
-    row1circle4.className = guessArray[3];
-    submitBtn.disabled = true;
+function freeSubmit() {
+    if (guess1El.className.includes('fill') && guess2El.className.includes('fill') && guess3El.className.includes('fill') && guess4El.className.includes('fill')) {submitBtnEl.disabled = false}
+}
 
+function submitGuess() {
+    count++;
+    console.log(count);
+    turnsCount.innerText = count;
+    getRow = document.getElementById(`row${count}`);
+    circle1 = getRow.children[0];
+    circle2 = getRow.children[1];
+    circle3 = getRow.children[2];
+    circle4 = getRow.children[3];
+    circle1.className = guess1El.className;
+    circle2.className = guess2El.className;
+    circle3.className = guess3El.className;
+    circle4.className = guess4El.className;
+    calculateWinner()
+}
 
-    // console.log('cCode', cCode);
-    // console.log('guessArray', guessArray);
-    if ((cCode[1] === guessArray[1]) && (cCode[2] === guessArray[2]) && (cCode[3] === guessArray[3]) && (cCode[4] === guessArray[4])) {console.log('yay')}
-  };
+function calculateWinner(){
+    submitBtnEl.disabled = true;
+    guess1El.className = 'guess';
+    guess2El.className = 'guess';
+    guess3El.className = 'guess';
+    guess4El.className = 'guess';
+    if (circle1.className === cGuess1.className && circle2.className=== cGuess2.className && circle3.className === cGuess3.className && circle4.className === cGuess4.className) {crackedCount.innerText++; alert('yay!')}
+    cArray();
+};
 
-        }
-    };
-
-
-
-
-
-
-// if (guessChoice.class != null) {console.log(guessChoice) }
-      // TODO: need to figure out how to make 'submitBtn.disabled = false' happen when all 4 guess circles are filled 
-  //  };
-   
-
-  //1. Grab all the circles
-//2. Iterate through each circle
-//3. On each iteration you want to check if the current circle has a color
-//3a. Pretend we are in an iteration:
-// console.log(circle.className)
-// if (circle.className.includes('red')) {
-//     console.log("Disable feature!")
-// }
-
-// function updateGuess(gNum) {
-
-// }
-
-// updateGuess();
-// handleBtnClick()
