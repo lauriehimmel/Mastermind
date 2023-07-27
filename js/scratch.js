dc6e6c6a18319597c355969a6e35e3c138cdfa19
laurhimms <lauriehimmel1@gmail.com>
2023-07-27 09:32:27 -0700

    update styling

--- js/scratch.js
+++ js/scratch.js
@@ -158,9 +158,9 @@ function calculateWinner(){
     guess2El.className = 'guess';
     guess3El.className = 'guess';
     guess4El.className = 'guess';
-    if (count <= 2 && circle1.className === cGuess1.className && circle2.className === cGuess2.className && circle3.className === cGuess3.className && circle4.className === cGuess4.className) {
+    if (count <= 5 && circle1.className === cGuess1.className && circle2.className === cGuess2.className && circle3.className === cGuess3.className && circle4.className === cGuess4.className) {
       winnerNotice();
-    } else if (count >= 2) {
+    } else if (count >= 5) {
       loserNotice();
     };
   };
