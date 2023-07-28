b43b39858a674691369b9d7a69c5442f48c8e89a
laurhimms <lauriehimmel1@gmail.com>
2023-07-27 22:12:57 -0700

    Final tweaks, including cleaning up readme doc

--- README.md
+++ README.md
@@ -7,19 +7,33 @@ Invented in the 70s by Mordecai Meirowitz, Mastermind was initially based on a p
 Though Mastermind is typically a two-player game, I will be making a one-player, one-computer version, where the player must guess the computer's randomly generated code. Will you find satisfaction in beating the bot? Or will your codebreaking attempts be thwarted?
 
 ## Gameplay - Screenshots
-![Wireframe screenshot](/wireframe3.png)
+![Wireframe screenshot](img/wireframe3.png)
+![Gameplay screenshot 2](img/Mastermind-screenshot-2.png)
+![Gameplay screenshot 3](img/Mastermind-screenshot-3.png)
+![Gameplay screenshot 4](img/Mastermind-screenshot-4.png)
 
 ## Technologies Used
 - HTML
 - CSS
 - JS
 
-## Key Resources
-
-
 ## Getting Started
-<!-- ☐ Getting Started: In this section include the link to your deployed game and any instructions you deem important. -->
+### Find my game [here!](https://lhimms.github.io/Mastermind/)  
+
+The computer has randomly generated a sequence of four colors. You must guess this sequence by clicking a color button and then selecting the circle you would like to place it in. Once you have four colors placed in the order you would like you guess, you can push the Submit button!
+
+As the peg hints are a future feature for me, you may view the computer's code in the console so you have the option to get a winner's message if guessing it too hard.
 
 
 ## Future Features
-<!-- ☐ Next Steps: Planned future enhancements (icebox items). -->
+1. **Hints**: The pegs are going to be a fantastic addition to this game - they will give the player hints to help guide them to the correct code.
+    
+    - A black peg indicates a color is correct, but in the wrong position.
+    - A white peg indicates a color is correct *and* in the correct position.
+    - No peg indicates a color has been guessed that is not featured in the code at all.    
+    
+    These clues, sparse though they may seem, greatly help the player guess.
+
+2. **Audio**: I would love to incorporate some Mission Impossible-esque music to really immerse the player in the world of the game. I would also like to add winning and losing sound notifications.
+
+3. **Animation**: I think there is a lot of room for more complex animations! I would love to see the instructions slide in, "pushing" the content below smoothly out of the way.
--- js/main.js
+++ js/main.js
@@ -1,37 +1,8 @@
-console.log('howdy!');
-
-
-/*----- constants -----*/
-
-
-
-// ******************
-// TO DO!!!!!! when you click the player guess circles before choosing a color, they disappear!!!
-// CPU CODE CHANGES BEFORE BEING REVEALED - don't do until click play again
-// look at this link for instructions button?? https://codepen.io/Saabbir/pen/LNzdGZ
-// ******************
-
-/*----- state variables -----*/
-
-let colorArray = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+let colorArray = ["red", "orange", "yellow", "green", "blue", "purple"];
 let cGuess1;
 let cGuess2;
 let cGuess3;
-let cGuess4;  
-
-function cArray() {let cArray =  [(colorArray[Math.floor(Math.random() * 5)+1]), (colorArray[Math.floor(Math.random() * 5)+1]), (colorArray[Math.floor(Math.random() * 5)+1]), (colorArray[Math.floor(Math.random() * 5)+1]), ];
-console.log(cArray);
-cGuess1 = document.querySelector('.cpuCode-box').children[0];
-cGuess2 = document.querySelector('.cpuCode-box').children[1];
-cGuess3 = document.querySelector('.cpuCode-box').children[2];
-cGuess4 = document.querySelector('.cpuCode-box').children[3];
-
-cGuess1.className = `fill-${cArray[0]}-circle`;
-cGuess2.className = `fill-${cArray[1]}-circle`;
-cGuess3.className = `fill-${cArray[2]}-circle`;
-cGuess4.className = `fill-${cArray[3]}-circle`;}
-
-cArray();
+let cGuess4;
 
 let targetClass;
 let target;
@@ -40,177 +11,216 @@ let newClassGuess1;
 let newClassGuess2;
 let newClassGuess3;
 let newClassGuess4;
-let count = 0;
 let getRow;
 let circle1;
 let circle2;
 let circle3;
 let circle4;
-// let newCrackedCount;
-
-
-/*----- cached elements  -----*/
-let turnsCount = document.getElementById('turnsCount');
-turnsCount.innerText = 0;
-
-// let crackedCount = document.getElementById('codesCracked');
-// crackedCount.innerText = 0;
-
-// let thwartedCount = document.getElementById('thwarted');
-// thwartedCount.innerText = 0;
-
-let colorButtonEls = document.querySelectorAll('.color-buttons button')
-
-let circleEls = document.querySelectorAll('.player-guesses .guess')
-
-let submitBtnEl = document.querySelector('.submit')
-
 let rowArray = [];
+let count = 0;
 
-let row1ArrayEls = document.querySelector('#row1').children;
-for (let i=0; i<4; i++) {
-  rowArray.push(row1ArrayEls[i]);
-};
-let row2ArrayEls = document.querySelector('#row2').children;
-for (let i=0; i<4; i++) {
-  rowArray.push(row2ArrayEls[i]);
-};
-let row3ArrayEls = document.querySelector('#row3').children;
-for (let i=0; i<4; i++) {
-  rowArray.push(row3ArrayEls[i]);
-};
-let row4ArrayEls = document.querySelector('#row4').children;
-for (let i=0; i<4; i++) {
-  rowArray.push(row4ArrayEls[i]);
-};
-let row5ArrayEls = document.querySelector('#row5').children;
-for (let i=0; i<4; i++) {
-  rowArray.push(row5ArrayEls[i]);
-};
-
-let codeReveal = document.querySelector('.cpuCode-box');
-
-let popup = document.querySelector('.hidden');
-
-let guess1El = (document.getElementById('guess1'));
-let guess2El = (document.getElementById('guess2')); 
-let guess3El = (document.getElementById('guess3'));
-let guess4El = (document.getElementById('guess4'));
-
-let yesPlayAgain = document.querySelector('.yesbutton');
-let noPlayAgain = document.querySelector('.nobutton');
-
-let goodbyeScreen = document.querySelector('.byehidden');
-
-let instrBtn = document.querySelector('.instruction-button');
+let turnsCount = document.getElementById("turnsCount");
+let colorButtonEls = document.querySelectorAll(".color-buttons button");
+let circleEls = document.querySelectorAll(".player-guesses .guess");
+let submitBtnEl = document.querySelector(".submit");
+let codeReveal = document.querySelector(".cpuCode-box");
+let popupEl = document.querySelector(".hidden");
+let popupElLose = document.querySelector(".hidden-lose");
+let guess1El = document.getElementById("guess1");
+let guess2El = document.getElementById("guess2");
+let guess3El = document.getElementById("guess3");
+let guess4El = document.getElementById("guess4");
+let yesPlayAgain = document.querySelector(".yesbutton");
+let yesPlayAgainLose = document.querySelector(".yesbutton-lose");
+let noPlayAgain = document.querySelector(".nobutton");
+let noPlayAgainLose = document.querySelector(".nobutton-lose");
+let goodbyeScreen = document.querySelector(".byehidden");
+let instrBtn = document.querySelector(".instruction-button");
 let instructionsEl = document.getElementById("hiddentext");
+let winnerMessageEl = document.getElementById("winnerhidden");
+let winLoseAlert = document.querySelector(".cpuBox-bg");
 
-let winnerMessageEl = document.getElementById('winnerhidden');
+let row1ArrayEls = document.querySelector("#row1").children;
+for (let i = 0; i < 4; i++) {
+  rowArray.push(row1ArrayEls[i]);
+}
+let row2ArrayEls = document.querySelector("#row2").children;
+for (let i = 0; i < 4; i++) {
+  rowArray.push(row2ArrayEls[i]);
+}
+let row3ArrayEls = document.querySelector("#row3").children;
+for (let i = 0; i < 4; i++) {
+  rowArray.push(row3ArrayEls[i]);
+}
+let row4ArrayEls = document.querySelector("#row4").children;
+for (let i = 0; i < 4; i++) {
+  rowArray.push(row4ArrayEls[i]);
+}
+let row5ArrayEls = document.querySelector("#row5").children;
+for (let i = 0; i < 4; i++) {
+  rowArray.push(row5ArrayEls[i]);
+}
 
-/*----- event listeners -----*/
 colorButtonEls.forEach((btn) => btn.addEventListener("click", handleBtnClick));
-submitBtnEl.addEventListener('click', submitGuess);
-yesPlayAgain.addEventListener('click', doPlayAgain);
-noPlayAgain.addEventListener('click', dontPlayAgain);
-instrBtn.addEventListener('click', toggleInstructions)
+submitBtnEl.addEventListener("click", submitGuess);
+yesPlayAgain.addEventListener("click", doPlayAgain);
+noPlayAgain.addEventListener("click", dontPlayAgain);
+instrBtn.addEventListener("click", toggleInstructions);
+yesPlayAgainLose.addEventListener("click", doPlayAgain);
+noPlayAgainLose.addEventListener("click", dontPlayAgain);
 
-/*----- functions -----*/
-disableSubmit()
+turnsCount.innerText = 0;
 instructionsEl.style.display = "none";
 
-function handleBtnClick(e) {
-    targetClass = e.target.getAttribute('class')
-    circleEls.forEach((circ) => circ.addEventListener('click', setColor))
+cArray();
+disableSubmit();
+
+function cArray() {
+  let cArray = [
+    colorArray[Math.floor(Math.random() * 5) + 1],
+    colorArray[Math.floor(Math.random() * 5) + 1],
+    colorArray[Math.floor(Math.random() * 5) + 1],
+    colorArray[Math.floor(Math.random() * 5) + 1],
+  ];
+  console.log(cArray);
+  cGuess1 = document.querySelector(".cpuCode-box").children[0];
+  cGuess2 = document.querySelector(".cpuCode-box").children[1];
+  cGuess3 = document.querySelector(".cpuCode-box").children[2];
+  cGuess4 = document.querySelector(".cpuCode-box").children[3];
+  cGuess1.className = `fill-${cArray[0]}-circle`;
+  cGuess2.className = `fill-${cArray[1]}-circle`;
+  cGuess3.className = `fill-${cArray[2]}-circle`;
+  cGuess4.className = `fill-${cArray[3]}-circle`;
 }
-
-function setColor(circ) {
-    circ.target.className = `${targetClass}-circle`;
-    freeSubmit()
-}
-
-function disableSubmit() {
-    submitBtnEl.disabled = true;
-}
-
-function toggleSubmit() {
-    if (submitBtnEl.disabled = true){submitBtnEl.id = 'disabled-button'} else {submitBtnEl.id = 'notdisabled'}
-}
-
-function freeSubmit() {
-    if (guess1El.className.includes('fill') && guess2El.className.includes('fill') && guess3El.className.includes('fill') && guess4El.className.includes('fill')) {submitBtnEl.disabled = false}
-}
-
-function submitGuess() {
-    count++;
-    console.log(count);
-    turnsCount.innerText = count;
-    getRow = document.getElementById(`row${count}`);
-    circle1 = getRow.children[0];
-    circle2 = getRow.children[1];
-    circle3 = getRow.children[2];
-    circle4 = getRow.children[3];
-    circle1.className = guess1El.className;
-    circle2.className = guess2El.className;
-    circle3.className = guess3El.className;
-    circle4.className = guess4El.className;
-    setTimeout(function(){calculateWinner()},10);
-}
-
-function calculateWinner(){
-    submitBtnEl.disabled = true;
-    guess1El.className = 'guess';
-    guess2El.className = 'guess';
-    guess3El.className = 'guess';
-    guess4El.className = 'guess';
-    if (count <= 5 && circle1.className === cGuess1.className && circle2.className === cGuess2.className && circle3.className === cGuess3.className && circle4.className === cGuess4.className) {
-      winnerNotice();
-    } else if (count >= 5) {
-      loserNotice();
-    };
-  };
-
-function winnerNotice() {
-  cArray();
-  setTimeout(function(){rowArray.forEach(function(row) {row.removeAttribute('class')})}, 500);
-  setTimeout(function(){playAgain()},500);
-};
-
-function loserNotice() {
-  // thwartedCount.innerText++; cArray(); 
-  count = 0; alert('MISSION FAILED. BACK TO SPY SCHOOL WITH YOU.');
-  rowArray.forEach(function(row) {row.removeAttribute('class')})
-  setTimeout(function(){revealCode()}, 2000)
-  setTimeout(function(){playAgain()}, 2000);
-};
-
-function playAgain() {
-  popup.style.display = 'flex';
-};
-
-function doPlayAgain() {
- init();
-};
-
-function dontPlayAgain() {
-  goodbyeScreen.style.display = 'flex';
-}
-
-function init() {
-  location.reload()
-};
-
-function revealCode() {
-  codeReveal.style.visibility = 'visible';
-};
 
 function toggleInstructions() {
   if (instructionsEl.style.display === "none") {
-    instructionsEl.style.display = "grid";
+    setTimeout(function () {
+      instructionsEl.style.display = "grid";
+    }, 450);
   } else {
-    instructionsEl.style.display = "none";
+    setTimeout(function () {
+      instructionsEl.style.display = "none";
+    }, 455);
   }
 }
 
+function handleBtnClick(e) {
+  targetClass = e.target.getAttribute("class");
+  circleEls.forEach((circ) => circ.addEventListener("click", setColor));
+}
 
+function setColor(circ) {
+  circ.target.className = `${targetClass}-circle`;
+  freeSubmit();
+}
 
+function disableSubmit() {
+  submitBtnEl.disabled = true;
+}
 
+function freeSubmit() {
+  if (
+    guess1El.className.includes("fill") &&
+    guess2El.className.includes("fill") &&
+    guess3El.className.includes("fill") &&
+    guess4El.className.includes("fill")
+  ) {
+    submitBtnEl.disabled = false;
+  }
+}
+
+function submitGuess() {
+  count++;
+  console.log(count);
+  turnsCount.innerText = count;
+  getRow = document.getElementById(`row${count}`);
+  circle1 = getRow.children[0];
+  circle2 = getRow.children[1];
+  circle3 = getRow.children[2];
+  circle4 = getRow.children[3];
+  circle1.className = guess1El.className;
+  circle2.className = guess2El.className;
+  circle3.className = guess3El.className;
+  circle4.className = guess4El.className;
+  setTimeout(function () {
+    calculateWinner();
+  }, 10);
+}
+
+function calculateWinner() {
+  submitBtnEl.disabled = true;
+  guess1El.className = "guess";
+  guess2El.className = "guess";
+  guess3El.className = "guess";
+  guess4El.className = "guess";
+  if (
+    count <= 5 &&
+    circle1.className === cGuess1.className &&
+    circle2.className === cGuess2.className &&
+    circle3.className === cGuess3.className &&
+    circle4.className === cGuess4.className
+  ) {
+    winnerNotice();
+  } else if (count >= 5) {
+    loserNotice();
+  }
+}
+
+function winnerNotice() {
+  setTimeout(function () {
+    revealCode();
+  }, 300);
+  setTimeout(function () {
+    playAgain();
+  }, 2000);
+  setTimeout(function () {
+    successColor();
+  }, 800);
+}
+
+function loserNotice() {
+  setTimeout(function () {
+    revealCode();
+  }, 300);
+  setTimeout(function () {
+    redAlert();
+  }, 800);
+  setTimeout(function () {
+    playAgain2();
+  }, 3000);
+}
+
+function playAgain() {
+  popupEl.style.display = "flex";
+}
+
+function playAgain2() {
+  popupElLose.style.display = "flex";
+}
+
+function doPlayAgain() {
+  init();
+}
+
+function dontPlayAgain() {
+  goodbyeScreen.style.display = "flex";
+}
+
+function init() {
+  location.reload();
+}
+
+function revealCode() {
+  codeReveal.style.visibility = "visible";
+}
+
+function redAlert() {
+  winLoseAlert.id = "lose-code";
+  codeReveal.id = "lose-code";
+}
+
+function successColor() {
+  winLoseAlert.id = "win-code";
+  codeReveal.id = "win-code";
+}
